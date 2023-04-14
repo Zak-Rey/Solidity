58f5bdcce829b47e488c79b70aada0aa05616e71
Zak-Rey <zaktripple@gmail.com>
2023-04-14 20:41:00 +0300

    Subcurrency example

--- Solana land/Currency.sol
+++ Solana land/Currency.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 contract Coin {
     address public minter;
-    mapping(address from, address to, uint amount);
+    mapping(address  => uint) public balances;
 
-    
+
 }
