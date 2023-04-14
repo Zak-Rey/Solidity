5c76057d46f0318de36efb1ef19463bab848b0cd
Zak-Rey <zaktripple@gmail.com>
2023-04-14 20:43:49 +0300

    Subcurrency example

--- Solana land/Currency.sol
+++ Solana land/Currency.sol
@@ -5,5 +5,16 @@ contract Coin {
     address public minter;
     mapping(address  => uint) public balances;
 
+    event Sent(address from, address to, uint amount);
 
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+    
 }
