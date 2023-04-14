5cc372c7da8ca2b0424d9e13e3a97b8ed0d8dba5
Zak-Rey <zaktripple@gmail.com>
2023-04-14 20:50:03 +0300

    Subcurrency example

--- Solana land/Currency.sol
+++ Solana land/Currency.sol
@@ -16,5 +16,18 @@ contract Coin {
         balances[receiver] += amount;
     }
 
-    
+    error insufficientBalance (uint requested, uint available);
+
+    //Sends an amount of existing coins from any caller to an address
+    function send(addres receiver, uint amount) {
+        if(amount > balances[msg.sender])
+            revert insufficientBalance({
+                requested: amount,
+                available: balance[msg.sender]
+            });
+        
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
 }
